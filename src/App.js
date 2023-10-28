dde2c1bc63c1e7e0b9df500e6dff456631542148
femmytedrey <femmytedrey@gmail.com>
2023-10-29 00:42:59 +0100

    in progress

--- src/App.js
+++ src/App.js
@@ -8,16 +8,73 @@ import './App.css';
 
 function App() {
   const [toDo, setToDo] = useState([
-    {id: "1", title: "Task 1", status: "false"},
-    {id: "2", title: "Task 2", status: "false"}
-  ])
+    // {id: "1", title: "Task 1", status: "false"},
+    // {id: "2", title: "Task 2", status: "false"},
+    {"id": 2, "title": "Task 2", "status": false},
+    {"id": 2, "title": "Task 2", "status": false}
+  ]);
 
   const [newTask, setNewTask] = useState('')
   const [updateData, setUpdateData] = useState('')
 
+  const addTask = () => {
+    
+  } 
+
+  /////////////////////////////////////////////////
+
+  const deleteTask = () => {
+
+  }
+
+  /////////////////////////////////////////////////
+
+  const markTask = (id) => {
+
+  }
+
+  /////////////////////////////////////////////////
+
+  const cancelUpdate = () => {
+
+  }
+
+  /////////////////////////////////////////////////
+
+  const changeTask = () => {
+
+  }
+
+  /////////////////////////////////////////////////
+
+  const updateTask = () => {
+
+  }
+
+  /////////////////////////////////////////////////
+
   return (
     <div className="container App">
       <h2>To Do List App (ReactJS)</h2>
+
+      {/* display ToDo */}
+
+      {toDo && toDo.length ? '' : 'No Task Added'}
+      {toDo && toDo
+        .map( (task, index) => {
+          return(
+            <React.Fragment key = {task.id}>
+              <div className='col taskBg'>
+                <div className={task.status ? 'done': ''}>
+                  <span className = 'taskNumber'>{index + 1}</span>
+                  <span className = 'taskText'>{task.title}</span>
+                </div>
+              </div>
+              
+            </React.Fragment>
+          )
+        })
+      }
     </div>
   );
 }
