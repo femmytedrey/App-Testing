ed8c00459447d0527a46289cc53948146abe8dca
femmytedrey <femmytedrey@gmail.com>
2023-10-28 08:09:13 +0100

    initialization

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faCircleCheck, faPen, faTrashCan
+} from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
 function App() {
+  const [toDo, setToDo] = useState([
+    {id: "1", title: "Task 1", status: "false"}
+    {id: "2", title: "Task 2", status: "false"}
+  ])
+
+  const [newTask, setNewTask] = useState('')
+  const [updateData, setUpdateData] = useState('')
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        
-      </header>
+    <div className="container App">
+      <h2>To Do List App (ReactJS)</h2>
     </div>
   );
 }
